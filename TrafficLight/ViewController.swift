cba2ee1455ce88ec28193a638b02926a5ad832fa
Artem Kriukov <art.kriukov@gmail.com>
2024-10-23 12:22:35 +0300

    Work with logic

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    enum Light {
+        case red
+        case yellow
+        case green
+    }
 
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var yellowLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    
+    var currentLight: Light = .green
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func changedLight(_ sender: UIButton) {
+        switch currentLight {
+        case .green:
+            currentLight = .red
+            redLight.alpha = 1.0
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 0.3
+        case .red :
+            currentLight = .yellow
+            yellowLight.alpha = 1.0
+            redLight.alpha = 0.3
+            greenLight.alpha = 0.3
+        case .yellow:
+            currentLight = .green
+            greenLight.alpha = 1.0
+            yellowLight.alpha = 0.3
+            redLight.alpha = 0.3
+        }
+        
+        sender.setTitle("Next", for: .normal)
+    }
 }
 
